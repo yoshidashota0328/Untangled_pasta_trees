da9b72d5102f9970b778446edf937ee3ecc72fcd
yoshidashota <shotayoshida0328@gmail.com>
2021-11-24 15:11:22 +0900

    ADD:parent_id can enter smaller than my own leaf_id

--- app/models/layer.rb
+++ app/models/layer.rb
@@ -2,4 +2,8 @@ class Layer < ApplicationRecord
   enum branch: { left: 0, left_down: 1, right: 2, right_down: 3 }
   belongs_to :tree
   belongs_to :user
+
+  validates :parent_id, presence: true
+  validates :positionX, presence: true
+  validates :positionY, presence: true
 end
