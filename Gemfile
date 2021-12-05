01e083c219b08ed3e9da29dbd91608ff932f1496
yoshidashota <shotayoshida0328@gmail.com>
2021-12-05 14:10:55 +0900

    CHANGE:all const to var

--- Gemfile
+++ Gemfile
@@ -32,12 +32,6 @@ gem 'jbuilder', '~> 2.5'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
-gem 'jquery-rails'
-gem 'gon'
-gem 'sorcery'
-gem 'rails-i18n'
-gem 'ransack'
-gem 'kaminari'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.1.0', require: false
 
@@ -70,3 +64,9 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'jquery-rails'
+gem 'gon'
+gem 'sorcery'
+gem 'rails-i18n'
+gem 'ransack'
+gem 'kaminari'
--- app/assets/javascripts/h1-anime.js
+++ app/assets/javascripts/h1-anime.js
@@ -1,7 +1,7 @@
 $(function () {
-  const before = $('.text');
-  const text = before.text(); // 文字をspanタグで囲む前のテキストを取得
-  const textArray = text.split('');  // 取得したテキストを1文字ずつに分割し配列にして変数に格納
+  var before = $('.text');
+  var text = before.text(); // 文字をspanタグで囲む前のテキストを取得
+  var textArray = text.split('');  // 取得したテキストを1文字ずつに分割し配列にして変数に格納
     
   let after = '';
   $.each(textArray,function(index,val){ // 配列の各文字をspanタグで囲み、繋げていく
@@ -10,16 +10,16 @@ $(function () {
     
   before.html(after);  // 元のテキストに生成したタグごと置き換え
     
-  const textcnt = textArray.length;
-  const circleR = ($('.circle').height()) / 2; // 円の半径
-  const fontH = ($('.inner').height());
-  const dist = circleR - fontH;
-  const animTime = $('.text').css('animation-duration').slice(0, -1);  // アニメーション時間を取得し、「s」を切り捨て
+  var textcnt = textArray.length;
+  var circleR = ($('.circle').height()) / 2; // 円の半径
+  var fontH = ($('.inner').height());
+  var dist = circleR - fontH;
+  var animTime = $('.text').css('animation-duration').slice(0, -1);  // アニメーション時間を取得し、「s」を切り捨て
     
   $('h1').each(function(index) {
-    const num = index + 1;
-    const radX = Math.sin(360 / textcnt * num * (Math.PI / 180)); // 文字の中心からのX軸方向の移動距離を計算
-    const radY = Math.sin((90 - (360 / textcnt * num)) * (Math.PI / 180)); // 文字の中心からのY軸方向の移動距離（実際にはマイナス移動）を計算
+    var num = index + 1;
+    var radX = Math.sin(360 / textcnt * num * (Math.PI / 180)); // 文字の中心からのX軸方向の移動距離を計算
+    var radY = Math.sin((90 - (360 / textcnt * num)) * (Math.PI / 180)); // 文字の中心からのY軸方向の移動距離（実際にはマイナス移動）を計算
     $(this).css({'transform': 'translate(' + dist * radX + 'px, ' + -(dist * radY) + 'px) rotateX(90deg) rotateY(' + 360 / textcnt * num + 'deg)', //1文字ずつ円形に等間隔で円形に配置
                 'animation-delay': animTime / textcnt * num + 's', // 1文字ずつアニメーション開始時間を遅延
                   });  //
