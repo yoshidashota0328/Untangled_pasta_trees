71587f2097b09f7b895bb80ca888af17e1498681
yoshidashota <shotayoshida0328@gmail.com>
2021-12-14 22:39:45 +0900

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,6 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'untangled-pasta-trees.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
