0fb360fa6152e8d3f4fbc311f10c5aeecf02d207
yoshidashota <shotayoshida0328@gmail.com>
2021-12-19 19:35:00 +0900

    ADD:how to use page link

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,6 +10,6 @@ class UserMailer < ApplicationMailer
   def reset_password_email(user)
     @user = User.find(user.id)
     @url = edit_password_reset_url(@user.reset_password_token)
-    mail(to: user.email, subject: 'Your password has been reset')
+    mail(to: user.email, subject: 'you can change the password')
   end
 end
